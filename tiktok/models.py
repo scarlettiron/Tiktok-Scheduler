26b595e845ec38215346fdc913db44c4b9467be2
scarlettiron <scottscarlett@gmail.com>
2023-01-14 01:44:06 -0500

    changed upload path

--- tiktok/models.py
+++ tiktok/models.py
@@ -4,7 +4,7 @@ from django.conf import settings
 User = settings.AUTH_USER_MODEL
 
 class TiktokVideo(models.Model):
-    file = models.FileField()
+    file = models.FileField(upload_to='videos/tiktok/')
     date = models.DateTimeField(auto_now_add=True)
     uploaded = models.BooleanField(default=False)
     scheduled = models.DateTimeField()
